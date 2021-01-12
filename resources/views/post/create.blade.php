7bf58891cae1591fb9f8fffb984ab1cf0379225c
Randy Steven <randysteven12@gmail.com>
2021-01-12 09:56:33 +0700

    validasi view

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -14,11 +14,21 @@
     </div>
     <div class="form-group">
         <label for="exampleFormControlInput1">Price</label>
-        <input type="text" class="form-control" name="price" id="exampleFormControlInput1" placeholder="Price">
-      </div>
+        <input type="text" class="form-control @error('price') is-invalid @enderror" name="price" id="exampleFormControlInput1" placeholder="Price">
+        @error('price')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+    </div>
     <div class="form-group">
       <label for="exampleFormControlTextarea1">Description</label>
-      <textarea class="form-control" id="exampleFormControlTextarea1" name="desc" rows="3"></textarea>
+      <textarea class="form-control @error('desc') is-invalid @enderror" id="exampleFormControlTextarea1" name="desc" rows="3"></textarea>
+        @error('desc')
+        <div class="invalid-feedback">
+            {{ $message }}
+            </div>
+        @enderror
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -7,17 +7,32 @@
         @method('PATCH')
         <div class="form-group">
           <label for="exampleFormControlInput1">Title</label>
-          <input type="text" class="form-control" name="title" value="{{ $post->title }}" id="exampleFormControlInput1" placeholder="Title">
+          <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ $post->title }}" id="exampleFormControlInput1" placeholder="Title">
+          @error('title')
+          <div class="invalid-feedback">
+              {{ $message }}
+          </div>
+      @enderror
         </div>
         <div class="form-group">
             <label for="exampleFormControlInput1">Price</label>
-            <input type="text" class="form-control" name="price" value="{{ $post->price }}" id="exampleFormControlInput1" placeholder="Price">
-          </div>
+            <input type="text" class="form-control @error('price') is-invalid @enderror" name="price" value="{{ $post->price }}" id="exampleFormControlInput1" placeholder="Price">
+            @error('price')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+        </div>
         <div class="form-group">
           <label for="exampleFormControlTextarea1">Description</label>
-          <textarea class="form-control" id="exampleFormControlTextarea1" name="desc" rows="3">
+          <textarea class="form-control @error('desc') is-invalid @enderror" id="exampleFormControlTextarea1" name="desc" rows="3">
               {{ $post->desc }}
           </textarea>
+          @error('desc')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
